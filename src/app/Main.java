144a351bff2764cf5f1c96f9fcde5d3ae83b6f0b
veronika-grbts <veranikadnepr@gmail.com>
2024-08-17 11:47:04 +0300

    add kilometers to miles convert

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,16 +2,22 @@ package app;
 
 public class Main {
     private static final  double CONV_M = 1.6093;
-
+    private static  final double CONV_K = 0.62137;
     public static void main(String[] args) {
         System.out.println("Converter App");
         double miles = 5;
+        double km = 10;
         double kilometers = convMilesToKilometers(miles);
+        double m = convKilometersToMiles(km);
 
-        System.out.println("Result is "+ kilometers + " kilometers");
+        System.out.println("Result is "+ kilometers + " kilometers and " + m + " miles");
     }
 
     private static double convMilesToKilometers(double miles){
         return miles * CONV_M;
+    }
+
+    private static double convKilometersToMiles(double km){
+        return km * CONV_K;
     }
 }
